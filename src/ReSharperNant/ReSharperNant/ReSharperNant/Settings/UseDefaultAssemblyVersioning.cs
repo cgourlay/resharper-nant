d0baf181751e44cf15b717f6c81326e3db55d79e
cgourlay <colin.gourlay5@gmail.com>
2014-07-27 01:33:16 +0100

    Added a type to manage settings.
    Added a type that will be used to determine if the default assembly
    versioning should be used or otherwise.

---
+++ src/ReSharperNant/ReSharperNant/ReSharperNant/Settings/UseDefaultAssemblyVersioning.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Settings;
+
+namespace ReSharperNant.Settings
+{
+    [SettingsKey(typeof(CodeInspectionSettings), "Use default assembly versioning settings")]
+    public class UseDefaultAssemblyVersioning
+    {
+        [SettingsEntry(true, "Enable default assembly versioning")]
+        public bool Enable { get; set; }
+    }
+}
